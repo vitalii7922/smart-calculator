4a2c462f81befef02c882c06b7517e92971363b6
Vitalii <https://github.com/hyperskill/intro-to-github.git>
2019-11-12 00:26:08 +0300

    commit

--- Problems/Which brackets are balanced/src/Main.java
+++ Problems/Which brackets are balanced/src/Main.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
 import java.util.Stack;
+/*
+Which brackets are balanced
 
+Given a string consisting of brackets, write a program to examine whether the pairs and the orders of "{", "}", "(", ")", "[", "]" are correct (balanced). For example, the program should print true for the string [()]{}{[()()]()} and false for ()[]}.
+
+The classic algorithm for solving this problem relies on using a stack.
+
+create an instance of a stack;
+traverse the input string;
+if the current character is a starting bracket "(" or "{" or "[" then push it to the stack;
+if the current is a closing bracket ")" or "}" or "]" then remove (pop) the top element from the stack; if the popped bracket is the matching starting bracket then fine else parenthesis are not balanced;
+after completing traversal, if there are some starting brackets left in the stack, then the parenthesis are not balanced.
+
+Sample Input:
+([][])
+Sample Output:
+true
+
+Sample Input:
+([](){([])})
+Sample Output:
+true
+
+Sample Input:
+{{[()]]
+Sample Output:
+false
+*/
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -21,7 +48,6 @@ class Main {
                 }
             }
         }
-
         System.out.println(stack.empty());
     }
 }
