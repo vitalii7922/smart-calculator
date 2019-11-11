0e0e6fb88a19ff1500cc42fa1b08c9dd3e21d740
Vitalii <https://github.com/hyperskill/intro-to-github.git>
2019-11-11 22:22:34 +0300

    commit

--- Problems/Which brackets are balanced/src/Main.java
+++ Problems/Which brackets are balanced/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+
+        String brackets = scanner.next();
+
+        for (int i = 0; i < brackets.length(); i++) {
+            if (brackets.charAt(i) == '(' || brackets.charAt(i) == '{'
+                    || brackets.charAt(i) == '['){
+                stack.push(brackets.charAt(i));
+            }else {
+                if (!stack.isEmpty()){
+                    stack.pop();
+                }else {
+                    System.out.println(false);
+                    return;
+                }
+            }
+        }
+
+        System.out.println(stack.empty());
     }
 }
